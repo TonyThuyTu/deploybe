e2823dabc0634e097aee792fabc0be6dca00f744
TonyThuyTu <quyngaisensua@gmail.com>
2025-08-24 21:45:35 +0700

    up

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ sequelize.authenticate()
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://deployfe-vtt9.onrender.com',
   credentials: true, // Cho phép gửi cookie qua trình duyệt
 }));
 
