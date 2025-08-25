4ee17b4e04538eb924435cd4b9a3c793027bd43e
TonyThuyTu <quyngaisensua@gmail.com>
2025-08-25 19:35:49 +0700

    up

--- config/redisClient.js
+++ config/redisClient.js
@@ -2,7 +2,7 @@ const { createClient } = require('redis');
 
 const redisClient = createClient({
   // url: 'redis://redis:6379', // ✅ sửa lại IP localhost
-   url: process.env.REDIS_URL, // sử dụng biến môi trường
+   url: process.env.REDIS_URL || 'redis://default:ctznwrNlh0fAQ5rppPuqEuyKUgD8xTrn@redis-17355.c270.us-east-1-3.ec2.redns.redis-cloud.com:17355', // sử dụng biến môi trường
 });
 
 redisClient.on('error', (err) => console.error('❌ Redis Client Error', err));
